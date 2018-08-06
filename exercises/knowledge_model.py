a47c676c8c3f098c2c628a011ac7b60b0e8af46a
aboud20-meet <aboud20@meet.mit.edu>
2018-08-06 10:34:02 +0300

    databases

--- exercises/knowledge_model.py
+++ exercises/knowledge_model.py
@@ -6,12 +6,14 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 class Knowledge(Base):
-	# Create a table with 4 columns
-	# The first column will be the primary key
-	# The second column should be a string representing
-	# the name of the Wiki article that you're referencing
-	# The third column will be a string representing the 
-	# topic of the article. The last column will be
-	# an integer, representing your rating of the article.
+	__tablename__="Knowledge"
+	id_number=Column(Integer,primary_key=True)
+	topic=Column(String)
+	name=Column(String)
+	rating=Column(Integer)
 
-	pass
+	def __repr__(self):
+		if self.rating<=7:
+			return("Unfortunately, this article does not have a better rating. Maybe, this is an article that should bereplaced soon")
+		else:
+			return(print("If you want to learn about", self.topic, ", we recommend you read the Wikipedia page", self.name,". We gave it a", self.rating, "out of 10!"))

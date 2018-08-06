a1061edd87d82da514bbc71921af485abe4e8684
aboud20-meet <aboud20@meet.mit.edu>
2018-08-06 14:09:02 +0300

    model & databases

--- exercises/knowledge_databases.py
+++ exercises/knowledge_databases.py
@@ -8,14 +8,45 @@ Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-def add_article():
-	pass
+session.query(Knowledge).delete()
+
+def add_article(name, topic, rating):
+	adding=Knowledge(
+		topic=topic,
+		name=name,
+		rating=rating)
+	session.add(adding)
+	session.commit()
+
+add_article("Rainbow", "Weather", 8 )
+add_article("Black holes", "Space", 10)
+add_article("Chocolate", "Food", 6)
 
 def query_all_articles():
-	pass
+	articles = session.query(Knowledge).all()
+	return articles
 
-def query_article_by_topic():
-	pass
+#print(query_all_articles())
+
+def query_article_by_topic(topic):
+	chosen_topic = session.query(
+       Knowledge).filter_by(
+       topic=topic).all()
+	return chosen_topic
+
+#print(query_article_by_topic("Weather"))
+
+def query_article_by_rating(threshhold):
+	articles=session.query(Knowledge).filter(Knowledge.rating<threshhold).all()
+	return articles
+
+# print(query_article_by_rating(7))
+
+def query_article_by_primary_key(searching):
+	chosen=session.query(Knowledge).filter_by(id_number=searching)
+	return chosen
+
+print(query_article_by_primary_key(1))
 
 def delete_article_by_topic():
 	pass
--- exercises/knowledge_model.py
+++ exercises/knowledge_model.py
@@ -13,7 +13,11 @@ class Knowledge(Base):
 	rating=Column(Integer)
 
 	def __repr__(self):
-		if self.rating<=7:
-			return("Unfortunately, this article does not have a better rating. Maybe, this is an article that should bereplaced soon")
-		else:
-			return(print("If you want to learn about", self.topic, ", we recommend you read the Wikipedia page", self.name,". We gave it a", self.rating, "out of 10!"))
+		return ("Name: {}\n"
+				"Topic: {}\n"
+				"Rating: {}\n"
+				"ID: {}").format(
+					self.name,
+					self.topic,
+					self.rating,
+					self.id_number)
